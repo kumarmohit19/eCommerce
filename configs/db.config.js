22d1136218802e4a9dfc608cb3869ed73a6d31ef
Kumar <m.kumarmohit19@gmail.com>
2022-04-21 18:09:27 +0530

    production db details updated

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,27 +1,28 @@
 module.exports = {
-    development: {
-        HOST: "localhost",
-        USER: "root",
-        PASSWORD: "Mohit@19",
-        DB: "ecom_db",
-        dialect: "mysql",
-        pool: {
-            max: 5,
-            min: 0,
-            acuire: 30000, //max time in ms that a pool will try to get connection before throwing error
-            idle: 10000  // maximum time in ms that a connection can be idle before being released
-        }
+  development: {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "Mohit@19",
+    DB: "ecom_db",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acuire: 30000, //max time in ms that a pool will try to get connection before throwing error
+      idle: 10000, // maximum time in ms that a connection can be idle before being released
     },
-    production: {
-        HOST: "sql6.freemysqlhosting.net",
-        USER: "sql6478840",
-        PASSWORD: "I3aSPgmBTw",
-        DB: "sql6478840",
-        dialect: "mysql",
-        pool: {
-            max: 5,
-            min: 0,
-            acuire: 30000, //max time in ms that a pool will try to get connection before throwing error
-            idle: 10000  // maximum time in ms that a connection can be idle before being released
-    }}
+  },
+  production: {
+    HOST: "relevel-ecommerce.cvyl0jb1nhb0.ap-south-1.rds.amazonaws.com",
+    USER: "relevel",
+    PASSWORD: "Ecommerce#2021",
+    DB: "ecom_db",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acuire: 30000, //max time in ms that a pool will try to get connection before throwing error
+      idle: 10000, // maximum time in ms that a connection can be idle before being released
+    },
+  },
 }
